1fc93f06fc9ca7f9b590a86b0f7d1d79d04f6687
ReiCat <reicat@gmail.com>
2025-01-14 03:11:23 +0200

    Fix TestXmlProcessor

--- backend/src/test/java/com/smit_test_task/backend/TestXmlProcessor.java
+++ backend/src/test/java/com/smit_test_task/backend/TestXmlProcessor.java
@@ -2,7 +2,6 @@ package com.smit_test_task.backend;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
 
 import java.util.Collections;
 import java.util.List;
@@ -62,18 +61,21 @@ public class TestXmlProcessor {
         assertEquals(mockSlot.getAvailable(), result.get(0).getAvailable());
     }
 
-    // @Test
-    // public void testProcessSlotsJsonReturnsDataResponse() throws Exception {
-    // String ID = "b6ce5c08-b94a-4678-aed8-1b4c5f8e571e";
-    // String time = "2025-01-07T06:00:00Z";
-    // String testPayload = String.format(
-    // "\"<tireChangeTimesResponse><availableTime><uuid>%s</uuid><time>%s</time></availableTime></tireChangeTimesResponse>\"",
-    // ID, time);
+    @Test
+    public void testProcessSlotsJsonReturnsDataResponse() throws Exception {
+        String ID = "b6ce5c08-b94a-4678-aed8-1b4c5f8e571e";
+        String time = "2025-01-07T06:00:00Z";
+        Boolean available = true;
+        String testPayload = String.format(
+                "<tireChangeTimesResponse><availableTime><uuid>%s</uuid><time>%s</time></availableTime></tireChangeTimesResponse>",
+                ID, time);
 
-    // List<Slot> result = xmlProcessor.processSlotsXml(testPayload);
+        Slot mockSlot = new Slot(ID, time, available);
 
-    // assertEquals(ID, result.get(0).getID());
-    // assertEquals(time, result.get(0).getTime());
-    // assertEquals(true, result.get(0).getAvailable());
-    // }
+        List<Slot> result = xmlProcessor.processSlotsXml(testPayload);
+
+        assertEquals(mockSlot.getID(), result.get(0).getID());
+        assertEquals(mockSlot.getTime(), result.get(0).getTime());
+        assertEquals(mockSlot.getAvailable(), result.get(0).getAvailable());
+    }
 }
